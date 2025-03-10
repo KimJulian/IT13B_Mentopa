899cf6fa0366e79e8b3d8540c11835837109e944
joberth <mentopakimjulian@gmail.com>
2025-03-10 11:04:04 +0800

    palindrome made

---
+++ Mentopa_IT13B_activities/Palindrome_Mentopa/src/Midterm/Palindrome_Mentopa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Midterm;
+
+/**
+ *
+ * @author User
+ */
+public class Palindrome_Mentopa {
+
+    public static void main(String[] args) {
+
+        String name = "Was it a car or a cat I saw";
+        for (int i = 26; i >= 0; i--) {
+            System.out.print(name.charAt(i) + " ");
+        }
+    }
+
+}
