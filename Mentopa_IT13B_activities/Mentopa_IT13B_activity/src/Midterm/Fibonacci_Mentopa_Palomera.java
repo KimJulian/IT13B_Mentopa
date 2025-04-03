e6299f2c48fed45ecd80a3f80dcece69942b3568
Mrgumball2004 <arjie1419@gmail.com>
2025-04-03 11:28:36 +0800

    ADDED FIBONACCI

---
+++ Mentopa_IT13B_activities/Mentopa_IT13B_activity/src/Midterm/Fibonacci_Mentopa_Palomera.java
@@ -0,0 +1,21 @@
+package Midterm;
+
+public class Fibonacci_Mentopa_Palomera {
+
+    public static void main(String[] args) {
+        int fibonacci = 10;
+        for (int a = 0; a < fibonacci; a++) {
+            System.out.print(fibonacci(a) + ",");
+        }
+    }
+
+    public static int fibonacci(int a) {
+        if (a == 0) {
+            return 0;
+        } else if (a == 1) {
+            return 1;
+        } else {
+            return fibonacci(a - 1) + fibonacci(a - 2);
+        }
+    }
+}
